fd4e419e1473dbd07ef4fd82d7ec8cc04070078e
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-06-17 09:48:48 -0500

    if else update and switch statement

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -5,11 +5,23 @@ var color = colors[Math.floor(Math.random()*colors.length)];
 
 var favorite = 'yellow'; // TODO: change this to your favorite color from the list
 
+console.log (color)
+
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 
 if (color == 'red') {
-	console.log ("I hate red and strawberries")
-}
+	console.log ("I hate red and strawberries");
+} else if (color == "orange") {
+	console.log ("Orange is the color of a carrot")
+} else if (color == "yellow") {
+	console.log ("Yellow is the color of the sun")
+} else if (color == "green") {
+	console.log ("Green is the color of grass")
+} else if (color == "blue") {
+	console.log ("Blue is the mood I'm in")
+} else if (color == "indigo" || "violet") {
+	console.log ("I don't know anything of that color")
+} 
 
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
