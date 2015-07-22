55e54ca4ccf543b1084d6206514629e1a0e41519
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-22 11:19:11 -0500

    form example with user input protection

--- public/projecteuler.php
+++ public/projecteuler.php
@@ -4,17 +4,18 @@ $findFactor = 13195;
 
 $arrayToCheck = [];
 
+$examplearray = [5, 7, 15, 19, 25];
+
+$isNotPrime = [];
+
 for ($i = 3; $i <= $findFactor; $i++){
 	if ($findFactor % $i == 0){
-		echo $i . PHP_EOL; 
+		// echo $i . PHP_EOL; 
 		array_push($arrayToCheck, $i); 
 	}
 }
 
-foreach ($arrayToCheck as $prime) {
-	
-	# code...
-}
+
 
 
 
